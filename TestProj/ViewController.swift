7cacc39b3fb6471e912633eefd5e907c61cba18b
DASARI SUDHIR BABU <sudhir.dasari0@achiever-115.local>
2019-07-25 10:54:29 +0530

    initialized and added comment and print statement

--- TestProj/ViewController.swift
+++ TestProj/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //this is my testProject For github
+        print("hello")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
